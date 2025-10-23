bcc2e72d9d75bb953179a8da557e582a9cf708c6
Maxime Guilbert <mx.guilbert@gmail.com>
2025-10-22 20:04:26 -0400

    wip affichage idées

--- middleware/authenticated.ts
+++
@@ -1,8 +0,0 @@
-import {getCurrentUser} from "vuefire";
-
-export default defineNuxtRouteMiddleware(async (to) => {
-    const user = await getCurrentUser()
-    if (!user) {
-        return navigateTo({ path: '/login', query: { redirect: to.fullPath } })
-    }
-})
